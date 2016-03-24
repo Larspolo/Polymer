3711adf34d41e594b8078d754d240c5ec0d8eb14
BrumesWolf <bram.stoker@student.hu.nl>
2016-03-24 12:34:25 +0100

    Les gemaakt volgens UML

--- src/model/Les.java
+++ src/model/Les.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Les {
+	private Date datum;
+	private Date beginTijd;
+	private Date eindTijd;
+	private String docent;
+	private String lokaal;
+	private String lesCode;
+	private String klasCode;
+	private ArrayList afwezigeStudenten;
+	
+	public Les(Date dT, Date bT, Date eT, String d, String lok, String les, String klas ){
+		datum = dT;
+		beginTijd = bT;
+		eindTijd = eT;
+		docent = d;
+		lokaal = lok;
+		lesCode = les;
+		klasCode = klas;
+	}
 	
 }
