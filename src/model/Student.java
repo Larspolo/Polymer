d1887ace104c3b54be826277f38f82141cd5188c
Victor <me@victorvos.com>
2016-03-24 11:27:29 +0100

    Student aangepast aan UML en Code Formatted

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,47 +1,57 @@
 package model;
 
 public class Student {
-	private String gebruikersNaam;
-	private String wachtwoord;
+    private String gebruikersNaam;
+    private String wachtwoord;
     private String voornaam;
     private String tussenvoegsel;
     private String beschrijving;
     private int studentNr;
     private static boolean ziek = false;
-	private Klas mijnKlas;
-	
-	public Student(String gbNm, String ww, String vn, String tvgsl, int snr, boolean zk) {
-		gebruikersNaam = gbNm;
-		wachtwoord = ww;
+    private Klas mijnKlas;
+
+    public Student(String gbNm, String ww, String vn, String tvgsl, int snr, boolean zk) {
+        gebruikersNaam = gbNm;
+        wachtwoord = ww;
         voornaam = vn;
         tussenvoegsel = tvgsl;
         studentNr = snr;
         ziek = zk;
-	}
-	
-	public String getGebruikersNaam() {
-		return gebruikersNaam;
-	}
+    }
 
-	public getWachtwoord() { return wachtwoord; }
+    public String getGebruikersNaam() {
+        return gebruikersNaam;
+    }
 
-    public void setWachtwoord(String w) { wachtwoord = w; }
+    public getWachtwoord() {
+        return wachtwoord;
+    }
 
-    public getStudentNr() { return studentNr; }
+    public void setWachtwoord(String w) {
+        wachtwoord = w;
+    }
 
-    public void toevoegenAfwezigheid(String bScVng){ beschrijving = bScVng; }
+    public getStudentNr() {
+        return studentNr;
+    }
 
-    public void setZiek(boolean zk){ziek = zk;}
+    public void toevoegenAfwezigheid(String bScVng) {
+        beschrijving = bScVng;
+    }
 
-	public boolean controleerWachtwoord(String ww) {
-		return ww.equals(wachtwoord);
-	}
-	
-	public void setMijnKlas(Klas k) {
-		mijnKlas = k;
-	}
-	
-	public Klas getMijnKlas() {
-		return mijnKlas;
-	}
+    public void setZiek(boolean zk) {
+        ziek = zk;
+    }
+
+    public boolean controleerWachtwoord(String ww) {
+        return ww.equals(wachtwoord);
+    }
+
+    public void setMijnKlas(Klas k) {
+        mijnKlas = k;
+    }
+
+    public Klas getMijnKlas() {
+        return mijnKlas;
+    }
 }
