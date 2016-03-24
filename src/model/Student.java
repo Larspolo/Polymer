fd2bff8e031df17d86dddfa7c232f2e8cf41ccfa
Victor <me@victorvos.com>
2016-03-24 11:23:09 +0100

    Klasse Opleiding aangemaakt en klasse Student verbeterd aan de hand van UML diagram

--- src/model/Student.java
+++ src/model/Student.java
@@ -3,17 +3,36 @@ package model;
 public class Student {
 	private String gebruikersNaam;
 	private String wachtwoord;
+    private String voornaam;
+    private String tussenvoegsel;
+    private String beschrijving;
+    private int studentNr;
+    private static boolean ziek = false;
 	private Klas mijnKlas;
 	
-	public Student(String gbNm, String ww) {
+	public Student(String gbNm, String ww, String vn, String tvgsl, int snr, boolean zk) {
 		gebruikersNaam = gbNm;
 		wachtwoord = ww;
+        voornaam = vn;
+        tussenvoegsel = tvgsl;
+        studentNr = snr;
+        ziek = zk;
 	}
 	
 	public String getGebruikersNaam() {
 		return gebruikersNaam;
 	}
-	
+
+	public getWachtwoord() { return wachtwoord; }
+
+    public void setWachtwoord(String w) { wachtwoord = w; }
+
+    public getStudentNr() { return studentNr; }
+
+    public void toevoegenAfwezigheid(String bScVng){ beschrijving = bScVng; }
+
+    public void setZiek(boolean zk){ziek = zk;}
+
 	public boolean controleerWachtwoord(String ww) {
 		return ww.equals(wachtwoord);
 	}
